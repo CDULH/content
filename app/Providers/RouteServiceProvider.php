73dc7fa44a79a93b61ae9785fd720328a49a3b27
CDlihang <1332675356@qq.com>
2018-07-23 15:44:03 +0800

    updated

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -85,7 +85,7 @@ class RouteServiceProvider extends ServiceProvider
     {
         Route::prefix('admin')
              ->middleware('admin')
-             ->namespace($this->namespace.'\admin')
+             ->namespace($this->namespace.'\Admin')
              ->group(base_path('routes/admin.php'));
     }
 }
